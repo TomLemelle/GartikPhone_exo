cea7c793e12eee917133550a1a32896d18d741fc
TomLemelle <tom.lemelle@hotmail.fr>
2021-10-16 02:27:34 +0200

    fix

--- Client/wwwroot/js/interop.js
+++ Client/wwwroot/js/interop.js
@@ -1,20 +1,4 @@
-﻿function createAlert() {
-    alert("Hey this is an alert");
-}
-
-function createPrompt(question) {
-    return prompt(question);
-}
-
-function setElementTextById(id, text) {
-    document.getElementById(id).innerText = text;
-}
-
-function focusOnElement(element) {
-    element.focus();
-}
-
-let canvas = null;
+﻿let canvas = null;
 
 function createPaintableCanvas(element) {
     canvas = new fabric.Canvas(element);
