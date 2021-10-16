1b3f606e19125967d72e2f54d9c23184c6d72732
TomLemelle <tom.lemelle@hotmail.fr>
2021-10-16 02:25:46 +0200

    add drawing system and brush size

---
+++ Client/wwwroot/js/interop.js
@@ -0,0 +1,43 @@
+﻿function createAlert() {
+    alert("Hey this is an alert");
+}
+
+function createPrompt(question) {
+    return prompt(question);
+}
+
+function setElementTextById(id, text) {
+    document.getElementById(id).innerText = text;
+}
+
+function focusOnElement(element) {
+    element.focus();
+}
+
+let canvas = null;
+
+function createPaintableCanvas(element) {
+    canvas = new fabric.Canvas(element);
+    canvas.isDrawingMode = true;
+    canvas.freeDrawingBrush.width = 2;
+    canvas.freeDrawingBrush.color = "#ff0000";
+    console.log(canvas);
+}
+
+function setBrushSizeTwo() {
+    document.getElementById('js-edit-two').addEventListener('click', () => {
+        canvas.freeDrawingBrush.width = 2;
+    })
+}
+
+function setBrushSizeFive() {
+    document.getElementById('js-edit-five').addEventListener('click', () => {
+        canvas.freeDrawingBrush.width = 5;
+    })
+}
+
+function setBrushSizeTen() {
+    document.getElementById('js-edit-ten').addEventListener('click', () => {
+        canvas.freeDrawingBrush.width = 10;
+    })
+}
