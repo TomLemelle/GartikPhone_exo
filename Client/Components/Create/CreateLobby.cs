3c6941147424f5c7bd61affd7eb30cd84a56dc0e
TomLemelle <tom.lemelle@hotmail.fr>
2021-10-17 03:54:52 +0200

    refactoring CreateLobbyForm component

---
+++ Client/Components/Create/CreateLobby.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace GartikPhone.Client.Components.Create
+{
+    public class CreateLobby
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long")]
+        public string Nickname { get; set; }
+        public string Round { get; set; }
+        public string RoomId { get; set; }
+    }
+}
